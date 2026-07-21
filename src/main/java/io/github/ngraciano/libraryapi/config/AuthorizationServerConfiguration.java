defbf25e4d53b7cc97fc4a1a639b9dd9ac5764ce
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-07-21 19:22:02 -0300

    add keys jwt

--- src/main/java/io/github/ngraciano/libraryapi/config/AuthorizationServerConfiguration.java
+++ src/main/java/io/github/ngraciano/libraryapi/config/AuthorizationServerConfiguration.java
@@ -1,6 +1,11 @@
 package io.github.ngraciano.libraryapi.config;
 
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -9,6 +14,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
@@ -16,7 +22,12 @@ import org.springframework.security.oauth2.server.authorization.settings.OAuth2T
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
+import java.util.UUID;
 
 @Configuration
 @EnableWebSecurity
@@ -50,6 +61,31 @@ public class AuthorizationServerConfiguration {
     @Bean
     public ClientSettings clientSettings(){
         return ClientSettings.builder().requireAuthorizationConsent(false).build();
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource()throws Exception{
+        RSAKey rsaKey= generateKeyRSA();
+        JWKSet jwkSet=new JWKSet(rsaKey);
+            return new ImmutableJWKSet<>(jwkSet);    
+    }
+
+    private RSAKey generateKeyRSA() throws Exception {
+        KeyPairGenerator keyPairGenerator= KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+
+        KeyPair keyPair=keyPairGenerator.generateKeyPair();
+
+        RSAPublicKey keyPublic=(RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey keyPrivate=(RSAPrivateKey) keyPair.getPrivate();
+
+
+        return new RSAKey.Builder(keyPublic).privateKey(keyPrivate).keyID(UUID.randomUUID().toString()).build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource){
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
     }
 
 }
