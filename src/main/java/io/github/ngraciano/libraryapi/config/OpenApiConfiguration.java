b39cb32b2112479bad9c2b8c3b69c046387768ca
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-07-23 16:00:17 -0300

    Swagger openApi init

---
+++ src/main/java/io/github/ngraciano/libraryapi/config/OpenApiConfiguration.java
@@ -0,0 +1,17 @@
+package io.github.ngraciano.libraryapi.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info=@Info(title = "Library API",version = "v1",contact = @Contact(name="Nicholas Graciano")
+))
+
+public class OpenApiConfiguration {
+
+
+}
